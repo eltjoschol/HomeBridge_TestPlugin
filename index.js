6390a1e41af4de089f75c58754bd96d2158ceb2a
Eltjo Schol <e.schol@eventresult.com>
2018-12-31 14:07:44 +0100

    .

--- index.js
+++ index.js
@@ -1,74 +1,38 @@
-const Service, Characteristic;
- 
-module.exports = function (homebridge) {
+use strict";
+
+var Service, Characteristic;
+
+module.exports = function(homebridge) {
+
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory("switch-plugin", "MyAwesomeSwitch", mySwitch);
-};
 
-mySwitch.prototype = {
-  getServices: function () {
-    let informationService = new Service.AccessoryInformation();
-    informationService
-      .setCharacteristic(Characteristic.Manufacturer, "My switch manufacturer")
-      .setCharacteristic(Characteristic.Model, "My switch model")
-      .setCharacteristic(Characteristic.SerialNumber, "123-456-789");
- 
-    let switchService = new Service.Switch("My switch");
-    switchService
-      .getCharacteristic(Characteristic.On)
-        .on('get', this.getSwitchOnCharacteristic.bind(this))
-        .on('set', this.setSwitchOnCharacteristic.bind(this));
- 
-    this.informationService = informationService;
-    this.switchService = switchService;
-    return [informationService, switchService];
-  }
-};
-
-
-const request = require('request');
-const url = require('url');
- 
-function mySwitch(log, config) {
-  this.log = log;
-  this.getUrl = url.parse(config['getUrl']);
-  this.postUrl = url.parse(config['postUrl']);
+  homebridge.registerAccessory("homebridge-dummy", "ACSSwitch", ACSSwitch);
 }
- 
-mySwitch.prototype = {
- 
-  getSwitchOnCharacteristic: function (next) {
-    const me = this;
-    request({
-        url: me.getUrl,
-        method: 'GET',
-    }, 
-    function (error, response, body) {
-      if (error) {
-        me.log('STATUS: ' + response.statusCode);
-        me.log(error.message);
-        return next(error);
-      }
-      return next(null, body.currentState);
-    });
-  },
-   
-  setSwitchOnCharacteristic: function (on, next) {
-    const me = this;
-    request({
-      url: me.postUrl,
-      body: {'targetState': on},
-      method: 'POST',
-      headers: {'Content-type': 'application/json'}
-    },
-    function (error, response) {
-      if (error) {
-        me.log('STATUS: ' + response.statusCode);
-        me.log(error.message);
-        return next(error);
-      }
-      return next();
-    });
+
+function ACSSwitch(log, config) {
+  this.log = log;
+  this.name = config.name;
+  this.stateful = config.stateful;
+
+  this._service = new Service.Switch(this.name);
+  this._service.getCharacteristic(Characteristic.On)
+    .on('set', this._setOn.bind(this));
+}
+
+ACSSwitch.prototype.getServices = function() {
+  return [this._service];
+}
+
+ACSSwitch.prototype._setOn = function(on, callback) {
+
+  this.log("Setting switch to " + on);
+
+  if (on && !this.stateful) {
+    setTimeout(function() {
+      this._service.setCharacteristic(Characteristic.On, false);
+    }.bind(this), 1000);
   }
-};
+
+  callback();
+}
