ad22d541534d8c2894ddec3646fcb4879d08c32b
Eltjo Schol <e.schol@eventresult.com>
2018-12-31 14:19:28 +0100

    .

--- index.js
+++ index.js
@@ -1,38 +1,62 @@
 "use strict";
 
-var Service, Characteristic;
+var Service, Characteristic, HomebridgeAPI;
 
 module.exports = function(homebridge) {
 
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-
-  homebridge.registerAccessory("homebridge-http-acs", "ACSSwitch", ACSSwitch);
+  HomebridgeAPI = homebridge;
+  homebridge.registerAccessory("homebridge-http-acs", "DummySwitch", DummySwitch);
 }
 
-function ACSSwitch(log, config) {
+function DummySwitch(log, config) {
   this.log = log;
   this.name = config.name;
   this.stateful = config.stateful;
-
+  this.reverse = config.reverse;
   this._service = new Service.Switch(this.name);
+  
+  this.cacheDirectory = HomebridgeAPI.user.persistPath();
+  this.storage = require('node-persist');
+  this.storage.initSync({dir:this.cacheDirectory, forgiveParseErrors: true});
+  
   this._service.getCharacteristic(Characteristic.On)
     .on('set', this._setOn.bind(this));
+
+  if (this.reverse) this._service.setCharacteristic(Characteristic.On, true);
+
+  if (this.stateful) {
+	var cachedState = this.storage.getItemSync(this.name);
+	if((cachedState === undefined) || (cachedState === false)) {
+		this._service.setCharacteristic(Characteristic.On, false);
+	} else {
+		this._service.setCharacteristic(Characteristic.On, true);
+	}
+  }
 }
 
-ACSSwitch.prototype.getServices = function() {
+DummySwitch.prototype.getServices = function() {
   return [this._service];
 }
 
-ACSSwitch.prototype._setOn = function(on, callback) {
+DummySwitch.prototype._setOn = function(on, callback) {
 
   this.log("Setting switch to " + on);
 
-  if (on && !this.stateful) {
+  if (on && !this.reverse && !this.stateful) {
     setTimeout(function() {
       this._service.setCharacteristic(Characteristic.On, false);
     }.bind(this), 1000);
+  } else if (!on && this.reverse && !this.stateful) {
+    setTimeout(function() {
+      this._service.setCharacteristic(Characteristic.On, true);
+    }.bind(this), 1000);
   }
-
+  
+  if (this.stateful) {
+	this.storage.setItemSync(this.name, on);
+  }
+  
   callback();
 }
